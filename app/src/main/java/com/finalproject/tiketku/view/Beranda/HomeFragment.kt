89ff27815c2ce827043849dacc6d7d9a6e45373c
MuchamadAndriyanto <mandriyanto9@gmail.com>
2023-06-10 12:59:19 +0700

    Menghubungkan nav_com pada bagian user

--- app/src/main/java/com/finalproject/tiketku/view/Beranda/HomeFragment.kt
+++ app/src/main/java/com/finalproject/tiketku/view/Beranda/HomeFragment.kt
@@ -37,7 +37,6 @@ class HomeFragment : Fragment() {
         return view
     }
 
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
--- app/src/main/java/com/finalproject/tiketku/view/User/RegisterFragment.kt
+++ app/src/main/java/com/finalproject/tiketku/view/User/RegisterFragment.kt
@@ -5,14 +5,28 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.finalproject.tiketku.R
+import com.finalproject.tiketku.databinding.FragmentRegisterBinding
 
 class RegisterFragment : Fragment() {
+    private lateinit var binding: FragmentRegisterBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_register, container, false)
+        binding = FragmentRegisterBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnRegister.setOnClickListener {
+            findNavController().navigate(R.id.action_registerFragment_to_loginFragment)
+        }
+
     }
 }
