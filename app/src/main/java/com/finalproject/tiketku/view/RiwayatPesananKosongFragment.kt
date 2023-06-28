5fd49be83b38d30c937e47df071f61cb9b7082b1
MuchamadAndriyanto <mandriyanto9@gmail.com>
2023-06-28 19:13:24 +0700

    Update pengecekan di riwayat

--- app/src/main/java/com/finalproject/tiketku/view/RiwayatPesananKosongFragment.kt
+++ app/src/main/java/com/finalproject/tiketku/view/RiwayatPesananKosongFragment.kt
@@ -6,19 +6,27 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.finalproject.tiketku.R
+import com.finalproject.tiketku.databinding.FragmentRiwayatPesananKosongBinding
+import com.finalproject.tiketku.databinding.FragmentSetClassBinding
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 
-class RiwayatPesananKosongFragment : Fragment() {
+class RiwayatPesananKosongFragment : BottomSheetDialogFragment() {
 
+    private lateinit var binding: FragmentRiwayatPesananKosongBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_riwayat_pesanan_kosong, container, false)
+        binding = FragmentRiwayatPesananKosongBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
 }
